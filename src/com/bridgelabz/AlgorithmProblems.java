d5f248bec27e9475a9e8f062faff67db5fbb27d5
Falguni Vashishtha <shrutisharma2302@gmail.com>
2022-08-16 15:01:41 +0530

    Insertion sort

--- src/com/bridgelabz/AlgorithmProblems.java
+++ src/com/bridgelabz/AlgorithmProblems.java
@@ -3,37 +3,36 @@ import java .util.ArrayList;
 import java.util.Scanner;
 
 public class AlgorithmProblems {
-    public static void main (String[]args) {
-        System.out.println("Enter your string: ");
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        int n = str.length(); //length of the string
-        AlgorithmProblems permutation = new AlgorithmProblems(); //Created object
-        permutation.permute(str);
-    }
-    private void permute (String str) {
-        if (str == null || str.length() == 0) {
-            return;
-        }
-        permutations(str.toCharArray(), 0);
-    }
-    //method to permute the string
-    private static void permutations(char[] chars, int currentIndex) {
-        if (currentIndex == chars.length - 1) {
-            System.out.println(String.valueOf(chars));
-        }
-        for (int i = currentIndex; i < chars.length; i++) {
-            swap(chars, currentIndex, i);
-            permutations(chars, currentIndex + 1);
-            swap(chars, currentIndex, i);
+    /*Method to sort array using insertion sort*/
+    void sort(int arr[]) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            /* Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
         }
     }
-    //method to swap
-    private static void swap(char[] chars, int i, int j)
-    {
-        char temp = chars[i];
-        chars[i] = chars[j];
-        chars[j] = temp;
+    /* A method to print array of size n*/
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for
+        (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+    // Main Method
+    public static void main(String args[]) {
+        int arr[] = { 4, 8, 77, 106, 1, 6, 99 };
+        AlgorithmProblems ob = new AlgorithmProblems(); //creating object
+        ob.sort(arr); //sorting the array
+        printArray(arr);
     }
 }
 
