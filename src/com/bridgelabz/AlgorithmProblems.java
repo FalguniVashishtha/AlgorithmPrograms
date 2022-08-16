6f0a9019ff7e01a1f72d3c5178180f214b00bfb5
Falguni Vashishtha <shrutisharma2302@gmail.com>
2022-08-16 16:20:39 +0530

    Check prime number are anagram and palindrome or not

--- src/com/bridgelabz/AlgorithmProblems.java
+++ src/com/bridgelabz/AlgorithmProblems.java
@@ -2,30 +2,60 @@ package com.bridgelabz;
 import java .util.ArrayList;
 import java.util.Scanner;
 import java.util.Arrays;
-public class AlgorithmProblems {
-    public static void main (String[]args)
+public class AlgorithmProblems
+{
+    public static void main(String[] args)
     {
-        int lower = 0, upper = 1000; //range from 0 to 1000
-        for (int i = lower; i <= upper; i++)
-            if (isPrime (i))
-                System.out.println (i);
+        int lowerLimit, upperLimit;
+        Scanner num = new Scanner(System.in);
+        System.out.println("Enter lower limit: ");
+        lowerLimit= num.nextInt();
+        System.out.println("Enter upper limit : ");
+        upperLimit = num.nextInt();
+
+        AlgorithmProblems range = new AlgorithmProblems();
+        System.out.println("The given range of prime number are anagram and palindrome or not? ");
+        range.primeRange(lowerLimit,upperLimit);
     }
-    //Method to check prime
-    static boolean isPrime (int n)
-    {
-        int count = 0;
-        // 0, 1 negative numbers are not prime
-        if (n < 2)
-            return false;
-        // checking the number of divisors b/w 1 and the number n-1
-        for (int i = 2; i < n; i++)
-        {
-            if (n % i == 0)
-                return false;
+    public void primeRange(int lower,int upper) {
+
+        for (int i = lower; i <= upper; i++) {
+            if (i == 1 || i == 0) {
+                continue;
+            }
+            int flag = 1;
+
+            for (int j = 2; j <= i / 2; j++)
+            {
+                if (i % j == 0)
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag == 1)
+            {
+                palindrome(i);
+            }
         }
-        // if the number is prime
-        return true;
     }
+    public static void palindrome(int number)
+    {
+
+        int reverse= 0, remainder;
+        int temp = number;
+        while (number > 0)
+        {
+            remainder = number % 10;
+            reverse = (reverse * 10) + remainder;
+            number = number / 10;
+        }
+        if (temp == reverse)
+        {
+            System.out.println(temp);
+        }
+    }
+
 
 }
 
