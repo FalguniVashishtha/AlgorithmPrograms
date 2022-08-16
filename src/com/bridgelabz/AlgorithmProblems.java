2cad7571adb5591df0aa77a690f299f9a01624a9
Falguni Vashishtha <shrutisharma2302@gmail.com>
2022-08-16 15:15:46 +0530

    Checking Prime number

--- src/com/bridgelabz/AlgorithmProblems.java
+++ src/com/bridgelabz/AlgorithmProblems.java
@@ -3,38 +3,28 @@ import java .util.ArrayList;
 import java.util.Scanner;
 import java.util.Arrays;
 public class AlgorithmProblems {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter two words to check for anagram.");
-        String str1 = sc.next();
-        String str2 = sc.next();
-
-        //converting to lower case
-
-        str1 = str1.toLowerCase();
-        str2 = str2.toLowerCase();
-        // check if length is same
-        if(str1.length() == str2.length()) {
-            // convert strings to char array
-            char[] charArray1 = str1.toCharArray();
-            char[] charArray2 = str2.toCharArray();
-            // sort the char array
-            Arrays.sort(charArray1);
-            Arrays.sort(charArray2);
-            /* if sorted char arrays are same
-             then the string is Anagram */
-            boolean result = Arrays.equals(charArray1, charArray2);
-            if(result)
-            {
-                System.out.println(str1 + " and " + str2 + " are anagram.");
-            }
-            else {
-                System.out.println(str1 + " and " + str2 + " are not anagram.");
-            }
+    public static void main (String[]args)
+    {
+        int lower = 0, upper = 1000; //range from 0 to 1000
+        for (int i = lower; i <= upper; i++)
+            if (isPrime (i))
+                System.out.println (i);
+    }
+    //Method to check prime
+    static boolean isPrime (int n)
+    {
+        int count = 0;
+        // 0, 1 negative numbers are not prime
+        if (n < 2)
+            return false;
+        // checking the number of divisors b/w 1 and the number n-1
+        for (int i = 2; i < n; i++)
+        {
+            if (n % i == 0)
+                return false;
         }
-        else {
-            System.out.println(str1 + " and " + str2 + " are not anagram.");
-        }
+        // if the number is prime
+        return true;
     }
 
 }
