64e07b195a7527d60f4fc02d75036f415335ee0a
Falguni Vashishtha <shrutisharma2302@gmail.com>
2022-08-18 12:06:47 +0530

    Find a Number

--- src/com/bridgelabz/AlgorithmProblems.java
+++ src/com/bridgelabz/AlgorithmProblems.java
@@ -4,58 +4,40 @@ import java.util.Scanner;
 import java.util.Arrays;
 public class AlgorithmProblems
 {
+    public static Scanner sc = new Scanner(System.in);
+    public void findNumber(int low, int high)
+    {
+
+        if(low == high) {
+            System.out.println("Your Number is : "+low);
+            System.out.println("Intermediatory Number is: "+(low-1)+" and "+(low+1));
+            return;
+        }
+
+        int middle = (low+high)/2;
+        int choice;
+        System.out.println("Press 1 if the number is between : "+low+" and "+middle);
+        System.out.println("Press 2 if the number is between : "+(middle+1)+" and "+high);
+        choice = sc.nextInt();
+
+        if(choice == 1) {
+            findNumber(low, middle);
+        }
+        else {
+            findNumber(middle+1, high);
+        }
+    }
+
     public static void main(String[] args)
     {
-        int lowerLimit, upperLimit;
-        Scanner num = new Scanner(System.in);
-        System.out.println("Enter lower limit: ");
-        lowerLimit= num.nextInt();
-        System.out.println("Enter upper limit : ");
-        upperLimit = num.nextInt();
 
-        AlgorithmProblems range = new AlgorithmProblems();
-        System.out.println("The given range of prime number are anagram and palindrome or not? ");
-        range.primeRange(lowerLimit,upperLimit);
+        AlgorithmProblems obj = new AlgorithmProblems();
+        System.out.println("Think of a number between 0 and 100");
+        int N = Integer.parseInt(args[0]);
+        obj.findNumber(1, N);
+        sc.close();
+
     }
-    public void primeRange(int lower,int upper) {
-
-        for (int i = lower; i <= upper; i++) {
-            if (i == 1 || i == 0) {
-                continue;
-            }
-            int flag = 1;
-
-            for (int j = 2; j <= i / 2; j++)
-            {
-                if (i % j == 0)
-                {
-                    flag = 0;
-                    break;
-                }
-            }
-            if (flag == 1)
-            {
-                palindrome(i);
-            }
-        }
-    }
-    public static void palindrome(int number)
-    {
-
-        int reverse= 0, remainder;
-        int temp = number;
-        while (number > 0)
-        {
-            remainder = number % 10;
-            reverse = (reverse * 10) + remainder;
-            number = number / 10;
-        }
-        if (temp == reverse)
-        {
-            System.out.println(temp);
-        }
-    }
-
 
 }
 
